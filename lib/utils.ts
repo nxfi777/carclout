d3d7051d745f1b12c5880cb885fd42c8a949427a
Rafael Aslanian <nafironato@DX-Pro.local>
2025-09-30 11:24:59 +0100

    .

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,15 +1,9 @@
-import { type ClassValue } from "clsx";
+import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { RecordId } from "surrealdb";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(
-    inputs
-      .flat()
-      .filter(Boolean)
-      .map((v) => String(v))
-      .join(" ")
-  );
+  return twMerge(clsx(inputs));
 }
 
 
